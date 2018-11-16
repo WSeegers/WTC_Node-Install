a615a500ebdfc9f958ecc9e55b16dd1aa3b2b2c4
WSeegers <wseegers.mauws@gmail.com>
2018-11-16 06:16:59 +0200

    Update node.sh
    Cleaned up

--- node.sh
+++ node.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 clear
-	echo "\n${CYAN}Downloading Nodejs into your Downloads Folder!${WHITE}"
+	echo "\nDownloading Nodejs into your Downloads Folder!"
 	
-				curl "https://nodejs.org/dist/v10.13.0/node-v10.13.0.tar.gz" | tar -xf - -C /goinfre/$USER/Downloads || exit 1
-				cd /goinfre/$USER/Downloads/node-v10.12.0
-				./configure
-				make -j4
-				echo "export PATH=$HOME/bin:"$PWD:$PWD/out/bin:$PWD/deps/npm/bin:$PATH >> ~/.zshrc
-				echo "alias npm='npm-cli.js'" >> ~/.zshrc
-				source ~/.zshrc
-				echo "Please open a new terminal"
-				break;;
+	curl "https://nodejs.org/dist/v10.13.0/node-v10.13.0.tar.gz" | tar -xf - -C /goinfre/$USER/Downloads || exit 1
+	cd /goinfre/$USER/Downloads/node-v10.13.0
+	./configure
+	make -j4
+	echo "" >> ~/.zshrc
+	echo "# Setup Node " >> ~/.zshrc
+	echo "export PATH=$HOME/bin:"$PWD:$PWD/out/bin:$PWD/deps/npm/bin:$PATH >> ~/.zshrc
+	echo "alias npm='npm-cli.js'" >> ~/.zshrc
+	source ~/.zshrc
 				
 
